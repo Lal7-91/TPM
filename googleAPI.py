35a53bca18c9f62a0e1c1ea1b34c030b3b453b26
Saleh Ahmed <Saleh.Ahmed.nb8@gmail.com>
2025-10-09 21:59:56 +0300

    just checked that serpapi work

--- googleAPI.py
+++ googleAPI.py
@@ -1,1 +1,21 @@
+from serpapi import GoogleSearch
+import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+params = {
+    "q": "Coffee",
+    "location": "Austin, Texas, United States",
+    "hl": "en",
+    "gl": "us",
+    "google_domain": "google.com",
+    "api_key": os.environ.get("SERPAPI_KEY")
+}
+
+search = GoogleSearch(params)
+results = search.get_dict()
+
+with open("test.json", "w") as f:
+    json.dump(results, f, indent=2)
